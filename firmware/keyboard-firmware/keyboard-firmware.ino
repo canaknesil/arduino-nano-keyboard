b02cd8359061f574353de78f8c7c8e492cb138fb
Can Aknesil <canaknesil@c-e10c225c.034-412-73746f7.bbcust.telenor.se>
2020-08-01 00:24:02 +0200

    Fixed out of bounds reference.

--- firmware/keyboard-firmware/keyboard-firmware.ino
+++ firmware/keyboard-firmware/keyboard-firmware.ino
@@ -14,8 +14,9 @@ void d_print(char *str, ...)
 {
 	static char msg[DEBUG_MSG_SIZE_MAX];
 	va_list args;
-	
-	str[DEBUG_MSG_SIZE_MAX - 1] = '\0'; // Chop str.
+
+	if (strlen(str) > DEBUG_MSG_SIZE_MAX - 1)
+		str[DEBUG_MSG_SIZE_MAX - 1] = '\0'; // Chop str.
 	
 	va_start(args, str);
 	vsprintf(msg, str, args);
